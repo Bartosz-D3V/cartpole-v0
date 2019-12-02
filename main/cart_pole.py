3f0b747bdcb7179f6674826b0e03bd8b9ca3dda6
Bartosz-D3V <nix.noxi379@gmail.com>
2019-12-02 07:16:10 +0000

    feat: update q_table function

--- main/cart_pole.py
+++ main/cart_pole.py
@@ -1,4 +1,11 @@
+import random
+
 import numpy as np
+
+from main import Action
+
+alpha = .1
+discount_factor = .2
 
 
 def discretize_state(observation):
@@ -9,11 +16,28 @@ def discretize_state(observation):
     return np.array([0, 0, pole_angle[mapped_pole_angle_index], pole_velocity[mapped_pole_velocity_index]])
 
 
+def random_action():
+    return random.choice(list(Action))
+
+
+def update_q_table(q_table, old_observation, observation, action, reward):
+    old_state = q_table[tuple(old_observation)][action.value]
+    new_state = q_table[tuple(observation)]
+    q_table[tuple(old_observation)][action.value] += alpha * (reward + discount_factor * np.max(new_state)) - old_state
+    return q_table
+
+
 def run_episode(env, q_table):
     env.reset()
     done = False
+    old_observation = discretize_state(env.state)
     while not done:
         env.render()
-        observation, reward, done, _ = env.step(0)
+        action = random_action()
+        observation, reward, done, _ = env.step(action.value)
         observation = discretize_state(observation)
+        q_table = update_q_table(q_table, old_observation, observation, action, reward)
+        old_observation = observation
+        print('x')
     env.close()
+    return q_table
