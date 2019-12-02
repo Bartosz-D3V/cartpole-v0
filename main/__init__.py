f024eae1b7477b212068f5ab79131d099ef2f3da
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2019-12-02 07:16:42 +0000

    feat: update q_table function (#2)

--- main/__init__.py
+++ main/__init__.py
@@ -6,9 +6,11 @@ from main.cart_pole import run_episode
 
 
 def main():
+    num_of_episodes = 1
     env = gym.make('CartPole-v1')
     q_table = np.zeros(((1, 1, 12, 12,) + (env.action_space.n,)))
-    run_episode(env, q_table)
+    for i in range(num_of_episodes):
+        q_table = run_episode(env, q_table)
 
 
 if __name__ == "__main__":
